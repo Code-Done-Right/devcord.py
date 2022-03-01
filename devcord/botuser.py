1834bc4b5779b53d1ace0613d15cded44d38c22b
Science Done Right <81095108+Code-Done-Right@users.noreply.github.com>
2022-03-01 19:48:47 +0530

    Added a bot file, events and listeners coming soon, tons of small issues :(

--- devcord/botuser.py
+++ devcord/botuser.py
@@ -1,6 +1,25 @@
+import asyncio
+import aiohttp
+
+from intents import Intents
+
 class BotUser:
-    def __init__(self, bot_token, intents, prefixes = None):
-        pass
+    def __init__(self, bot_token, intents = Intents.Standard, prefixes = None):
+        self.bot_token = bot_token
+        self.intents = intents
+        self.prefixes = prefixes
+        self.API_URL = f'https://discord.com/api/v9'
+
+        self.http_connection = None
+
+    # I have no freaking clue, I will get back with this
+    
+    # async def __create_new_command(self, connection):
+    #     async with aiohttp.ClientSession as session:
+    #         data = {
+
+    #         }
+    #         session.post()
 
     async def run(self):
         """
