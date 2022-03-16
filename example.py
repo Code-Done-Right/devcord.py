f9fa41a004b513ca194d0193287918c8090061ba
Code Done Right <81095108+Code-Done-Right@users.noreply.github.com>
2022-03-16 20:15:08 +0530

    Updated the expected code in the example file

--- example.py
+++ example.py
@@ -1,15 +1,15 @@
 import devcord
 from devcord.command_implements import SlashCommand, PrefixCommand
 
-INTENTS = devcord.intents.All_Enabled()
+INTENTS = devcord.intents.All()
 
 bot = devcord.BotUser(
-        bot_token = '...',
-        intents = INTENTS,
-        prefixes = ['devcord', 'devcord.']
-    )
+    bot_token = '...',
+    intents = INTENTS,
+    prefixes = ['devcord', 'devcord.']
+)
 
-@bot.command(implements = [SlashCommand, PrefixCommand])
+@bot.command(implements=[SlashCommand, PrefixCommand])
 async def ping(ctx):
     await ctx.reply('Pong!')
 
